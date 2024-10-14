757470783798abc99db6f842712dc737c8d24677
KHALID <96749372+a0zhar@users.noreply.github.com>
2024-10-14 15:49:41 +0200

    Update rgutils.h

--- ropgadgets/include/rgutils.h
+++ ropgadgets/include/rgutils.h
@@ -3,9 +3,6 @@
 #define _RGUTILS_HH
 #include "gadgets.h"
 
-// Adjust based on your gadget structure size
-#define MAX_GADGET_SIZE 256
-
 Gadget* delete_duplicate_gadgets(Gadget* current_gadgets, size_t* unique_count);
 void alpha_sort_gadgets(Gadget* gadgets, size_t count);
 
